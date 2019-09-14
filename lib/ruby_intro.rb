828304e63e1075b5fa6786738b8c2d51a09989fe
EC2 Default User <ec2-user@ip-172-31-17-57.us-east-2.compute.internal>
2019-09-14 15:34:16 +0000

    finished part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,63 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arrSum = 0
+  arr.each{|a| arrSum += a}
+  return arrSum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty? then
+    return 0;
+  end
+  
+  if arr.length == 1 then
+    return arr[0]
+  end
+  
+  # sort largest and smallest first two
+  num1 = [arr[0], arr[1]].max
+  num2 = [arr[0], arr[1]].min
+  
+  # start at 2 to skip these two
+  index = 2
+  
+  # iter over the rest of the array
+  while index < arr.length
+    # if bigger than the biggest number, replace it and bump that number down to second biggest
+    # elsif only bigger than the second biggest number, replace it
+    if arr[index] >= num1 then
+      num2 = num1
+      num1 = arr[index]
+    elsif arr[index] >= num2 then
+      num2 = arr[index]
+    end
+    index += 1
+  end
+  
+  return num1 + num2
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  
+  if arr.empty? then
+    return false
+  end
+  
+  outIndex = 0
+  inIndex = 0
+  
+  while outIndex < arr.length
+    inIndex = outIndex + 1
+    while inIndex < arr.length
+      if arr[outIndex] + arr[inIndex] == n then
+        return true
+      end
+      inIndex += 1
+    end
+    outIndex += 1
+  end
+  return false
 end
 
 # Part 2
